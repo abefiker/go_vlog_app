9d129d240260439285f2a464d9a4d3fbc778edd2
abfiker <new94abemelek@gmail.com>
2024-03-07 20:43:31 +0300

    "configuring HTTPS settings"

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -58,10 +59,15 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+		}
+		
 	srv := http.Server{
 		Addr:     *addr,
 		Handler:  app.routes(),
 		ErrorLog: errorLog,
+		TLSConfig: tlsConfig,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
